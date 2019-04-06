dd01180ed68c06aa70e1e1e157b81c76e8264c95
nian.zhou <nian.zhou@office.feng.com>
2019-04-06 18:13:12 +0800

    完成根据权限显示功能

--- assets/module/admin.js
+++ assets/module/admin.js
@@ -15,14 +15,32 @@ layui.define(['layer'], function (exports) {
                 value: access_token
             });
         },
+        //缓存角色
+        putRole: function (role) {
+            layui.data(admin.getTableName(), {
+                key: 'role',
+                value: role
+            });
+        },
         // 获取缓存的token
         getToken: function () {
             return layui.data(admin.getTableName()).access_token;
+        },
+        //获取缓存的角色
+        getRole: function () {
+            return layui.data(admin.getTableName()).role;
         },
         // 清除token
         removeToken: function () {
             layui.data(admin.getTableName(), {
                 key: 'access_token',
+                remove: true
+            });
+        },
+        //清除角色缓存
+        removeRole: function () {
+            layui.data(admin.getTableName(), {
+                key: 'role',
                 remove: true
             });
         },
@@ -40,77 +58,6 @@ layui.define(['layer'], function (exports) {
                 success: success
             });
         },
-        // // 封装ajax请求
-        // ajax: function (param) {
-        //     var successCallback = param.success;
-        //     param.success = function (result, status, xhr) {
-        //         // 判断登录过期和没有权限
-        //         var jsonRs;
-        //         if ('json' == param.dataType.toLowerCase()) {
-        //             jsonRs = result;
-        //         } else {
-        //             jsonRs = admin.parseJSON(result);
-        //         }
-        //         if (jsonRs && admin.ajaxSuccessBefore(jsonRs) == false) {
-        //             return;
-        //         }
-        //         successCallback(result, status, xhr);
-        //     };
-        //     param.error = function (xhr) {
-        //         param.success({code: xhr.status, msg: xhr.statusText});
-        //     };
-        //     param.beforeSend = function (xhr) {
-        //         /*var headers = admin.getAjaxHeaders();
-        //         for (var i = 0; i < headers.length; i++) {
-        //             xhr.setRequestHeader(headers[i].name, headers[i].value);
-        //         }*/
-        //     };
-        //     if (admin.getToken()) {
-        //         param.data.access_token = admin.getToken();
-        //     }
-        //     $.ajax(param);
-        // },
-        // // ajax自动传递header
-        // getAjaxHeaders: function () {
-        //     var headers = [];
-        //     var access_token = admin.getToken();
-        //     if (access_token) {
-        //         headers.push({
-        //             name: 'Authorization',
-        //             value: 'Bearer ' + access_token
-        //         });
-        //     }
-        //     return headers;
-        // },
-        // // ajax请求结束后的处理，返回false阻止代码执行
-        // ajaxSuccessBefore: function (res) {
-        //     if (res.status.code == 4) {
-        //         admin.removeToken();
-        //         layer.msg('登录过期', {icon: 2, time: 1500}, function () {
-        //             top.location.reload();
-        //         });
-        //         return false;
-        //     } else if (res.code == 403) {
-        //         layer.msg('没有访问权限', {icon: 2});
-        //     } else if (res.code == 404) {
-        //         layer.msg('目标不存在(404)', {icon: 2});
-        //     }
-        //     return true;
-        // },
-        // // 转换json
-        // parseJSON: function (str) {
-        //     if (typeof str == 'string') {
-        //         try {
-        //             var obj = JSON.parse(str);
-        //             if (typeof obj == 'object' && obj) {
-        //                 return obj;
-        //             }
-        //         } catch (e) {
-        //             console.error(e);
-        //         }
-        //     }
-        // },
-
     };
 
     // 所有ew-event
